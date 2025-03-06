ceb72ca215372aa4010488477a3dacdfb6887666
Benny Skov <benny.skov@kyndryl.com>
2025-03-06 14:42:26 +0100

    Update sa_redirect_tmp.sh

--- opsware-agent-linux/sa_redirect_tmp.sh
+++ opsware-agent-linux/sa_redirect_tmp.sh
@@ -5,5 +5,6 @@ echo cogbot.tmp_dir: /var/opt/opsware/tmp>>$ocfg
 mkdir -p /var/opt/opsware/tmp
 chmod 0744 /var/opt/opsware/tmp
 cat $ocfg
-[ -f /sbin/service ] && /sbin/service opsware-agent restart || /bin/systemctl restart opsware-agent.service
-echo sa_redirect_tmp completed.   
+[ -f /sbin/service ] && /sbin/service opsware-agent restart 
+[ -f /etc/rc.d/init.d ] && /etc/rc.d/init.d/opsware-agent restart
+echo opsware_sa_redirect_tmp completed.   
